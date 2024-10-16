af2807efe2105b94bcaf3842bcb3f4a0a3ef2e9c
Jhonny <jhonnycgarcia@gmail.com>
2024-10-16 16:25:47 -0500

    Obtener países por región

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'countries-selector-page',
@@ -31,6 +32,9 @@ export class SelectorPageComponent implements OnInit {
 
   onRegionChange(): void {
     this.myForm.get('region')!.valueChanges
+      .pipe(
+        switchMap((region) => this.countriesService.getCountriesByRegion(region as Region))
+      )
       .subscribe((region) => {
         console.log('Region changed to: ', region);
       });
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Region, SmallCountry } from '../interfaces/country.interface';
+import { catchError, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
+
+  private baseUrl: string = 'https://restcountries.com/v3.1';
 
   private _regions: Region[] = [
     Region.Africa,
@@ -14,14 +19,21 @@ export class CountriesService {
     Region.Oceania,
   ];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get regions(): Region[] {
     return [...this._regions];
   }
 
-  getCountriesByRegion(region: Region): SmallCountry[] {
-    return [];
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if(!region) return of([]);
+    const url = `${this.baseUrl}/region/${region}?fields=name,cca3,borders`;
+    return this.http.get<SmallCountry[]>(url)
+      .pipe(
+        catchError(() => of([]))
+      );
   }
 
 }
